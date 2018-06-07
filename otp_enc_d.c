f12437472963adc2455b50d34dc62ba55d84291b
Cihan Adrian Towery <adrian.towery@gmail.com>
2018-06-06 17:50:04 -0700

    copy paste client.c into enc

--- otp_enc_d.c
+++ otp_enc_d.c
@@ -14,18 +14,21 @@
 #define MAX 92
 #define SPACE 32
 
-static void handleRequest(int);
+void encrypt(char[], char[], int);
+
 
 int main(int argc, char* argv[]){
 
 	//from server.c
 	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
 	int running = 1;
-	int* yes =1;
+	int* yes;
+	char* key;
+    char text[BUF_SIZE];
 	socklen_t sizeOfClientInfo;
 	char buffer[256];
 	struct sockaddr_in serverAddress, clientAddress;
-	spawnPid_t spawnPid;
+	pid_t spawnPid;
 	
 	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
 	setsockopt(listenSocketFD, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
@@ -63,27 +66,38 @@ int main(int argc, char* argv[]){
         spawnPid = fork();
 		switch (spawnPid){
 			
-			case -1:
-            	error("ERROR on fork");
-            	exit(1);
-            case 0:
-				memset(buffer, '\0', 256);
-            	read(listenSocketFD, buffer, sizeof(buffer));
-            	////DO MORE STUFF HERE KEEP LOOPING TIL WE READ IT ALL
-            	while ( data > 0) {
-					// Get the message from the client and display it
-					charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
-					if (charsRead < 0) error("ERROR reading from socket");
-					else if (charsRead == 0) break;
+		case -1:
+        	error("ERROR on fork");
+        	exit(1);
+        case 0:
+			memset(buffer, '\0', 256);
+        	read(listenSocketFD, buffer, sizeof(buffer));
+        	////DO MORE STUFF HERE KEEP LOOPING TIL WE READ IT ALL
+        	char * readBuffer = buffer; //im not sure why this is needed but when i added this it worked
+        	while (running) {
+				// Get the message from the client and display it
+				charsRead = recv(establishedConnectionFD, readBuffer, 255, 0); // Read the client's message from the socket
+				if (charsRead < 0) {error("ERROR reading from socket"); exit(1);}
+				else if (charsRead == 0) break;
 
-					// Send a Success message back to the client
-					charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
-					if (charsRead < 0) error("ERROR writing to socket");
-					close(establishedConnectionFD); // Close the existing socket which is connected to the client
+				//search for enf of file
+				int i;
+				for (i=0; i< charsRead; i++) {
 
-            	}
-            	encrypt(text, key, strlen(text)); //Encrypt it using earlier function
+					//DO STUFF HERE?
+				}
 
+				// Send a Success message back to the client
+				charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
+				if (charsRead < 0) error("ERROR writing to socket");
+				close(establishedConnectionFD); // Close the existing socket which is connected to the client
+
+        	}
+        	//start with a blank chararray, copy as much as the 
+            memset(text, '/0', sizeof(text));
+            strncpy(text, buffer, key-buffer);    
+           	encrypt(text, key, strlen(text));
+            write(establishedConnectionFD, text, sizeof(text));
         }
 	}	
 	close(listenSocketFD); // Close the listening socket
@@ -91,20 +105,25 @@ int main(int argc, char* argv[]){
 }
 
 
-void handleRequest(int cfd)
-{
-	char buf[BUF_SIZE];
- 	ssize_t numRead;
- 	while ((numRead = read(cfd, buf, BUF_SIZE)) > 0) {
-		if (write(cfd, buf, numRead) != numRead) {
-			syslog(LOG_ERR, "write() failed: %s", strerror(errno));
-			exit(EXIT_FAILURE);
+void encrypt(char text[], char key[], int l) {    //Function prototypes to avoid errors
+	int i, plain, encoded, decoderRing;
+	for(i=0;i<l-1;i++){
+		plain = text[i];
+		decoderRing = key[i];
+		//change them to our ascii range
+		if(plain == SPACE) plain = 91;
+		if(decoderRing == SPACE) decoderRing = 91;
+		encoded = plain + decoderRing;
+		printf("encoded is %d\n", encoded);
+		if (encoded > 91) { 
+			//subtract by range to get it back down to where it needs to be
+			encoded = encoded - (MAX-MIN);
+			printf("encoded is changed to %d\n", encoded);
 		}
+		//THEN change to a space.. this little bug got me for a while
+		if (encoded == 91) encoded = SPACE;
 	}
-	if (numRead == -1) {
-		syslog(LOG_ERR, "Error from read(): %s", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
+
 }
 
 
